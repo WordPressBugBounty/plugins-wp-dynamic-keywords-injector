f71e63e42d2bb66c011c18db747ecd95dc1981e0
Sean Murphy <sean@iamseanmurphy.com>
2026-06-30 07:39:54 +0000

    2.3.28

--- wp-dynamic-keywords-injector/wp-dynamic-keywords-injector.php
+++ wp-dynamic-keywords-injector/wp-dynamic-keywords-injector.php
@@ -3,11 +3,11 @@
  * Plugin Name: WP Dynamic Keywords Injector
  * Plugin URI: https://wordpress.org/plugins/wp-dynamic-keywords-injector/
  * Description: WP Dynamic Keywords Injector inserts dynamic keywords, spintax, page title and meta title.
- * Version: 2.3.27
+ * Version: 2.3.28
  * Author: Seerox
  * Author URI: https://www.seerox.com
  * Requires at least: 3.8
- * Tested up to: 6.9
+ * Tested up to: 7.0
  */
 
 /**
